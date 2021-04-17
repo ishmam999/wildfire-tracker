5ff1f288f72929149af1329302ea46d8b4734bdc
ishmam999 <iammahim@hotmail.com>
2021-04-18 06:59:33 +0900

    MarkerInfo has been included

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,10 +1,14 @@
+import { useState } from 'react'
 import GoogleMapReact from 'google-map-react'
 import LocationMarker from './LocationMarker'
+import LocationInfoBox from './LocationInfoBox'
 
 const Map = ({ eventData, center, zoom }) => {
+    const [locationInfo, setLocationInfo] = useState(null)
+
     const markers = eventData.map(ev => {
         if(ev.categories[0].id === 8) {
-            return <LocationMarker lat={ev.geometries[0].coordinates[1]} lng={ev.geometries[0].coordinates[0]} />
+            return <LocationMarker lat={ev.geometries[0].coordinates[1]} lng={ev.geometries[0].coordinates[0]} onClick={() => setLocationInfo({ id: ev.id, title: ev.title})}/>
         }
         return null
     })
@@ -17,16 +21,17 @@ const Map = ({ eventData, center, zoom }) => {
             >
                 {markers}
             </GoogleMapReact>
+            {locationInfo && <LocationInfoBox info={locationInfo} />}
         </div>
     )
 }
 
 Map.defaultProps = {
     center: {
-        // lat: 34.67202964,
-        // lng: 133.9170865
-        lat: 42.3265,
-        lng: -122.8756
+        lat: 34.67202964,
+        lng: 133.9170865
+        // lat: 42.3265,
+        // lng: -122.8756
     },
     zoom: 6
 }
